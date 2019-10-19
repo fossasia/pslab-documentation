884a314293a589cbf0af89a95063dbdac045349e
Marco A. Gutierrez <marcogg@marcogg.com>
2019-10-19 18:26:48 +0800

    fixing theme to default

--- source/conf.py
+++ source/conf.py
@@ -14,6 +14,11 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import sphinx_rtd_theme
+
+extensions = [
+    "sphinx_rtd_theme",
+]
 
 # -- Project information -----------------------------------------------------
 
@@ -47,7 +52,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'master'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
